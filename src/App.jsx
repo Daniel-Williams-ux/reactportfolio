32843b3e025eace8105de19a8877fc2b1ad119c8
Daniel Williams <williamsoligodaniel@gmail.com>
2023-03-28 16:31:15 +0000

    create scss file, and update app file with router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Layout from './components/Layout'
+import Portfolio from './components/Portfolio'
+import Dashboard from './components/Dashboard'
+import './App.scss'
 
 export default function App() {
   return (
-    <main>
-      React ⚛️ + Vite ⚡ + Replit 🌀
-    </main>
+     <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
